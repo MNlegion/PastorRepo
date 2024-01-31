a42e3a513fc7c7d61ca4e088a95257564882404f
MNlegion <rkreuser30@gmail.com>
2024-01-31 17:25:49 -0500

    controller function setup for routes

---
+++ backend/controllers/postController.js
@@ -0,0 +1,35 @@
+// @desc    Get all posts
+// @route   GET /api/posts
+// @access  Private
+const getPosts = (req, res) => {
+  res.status(200).json({ message: "Get Post" });
+};
+
+// @desc    Create Post
+// @route   POST /api/posts
+// @access  Private
+const createPost = (req, res) => {
+  res.status(200).json({ message: "Create Post" });
+};
+
+// @desc    Update Post
+// @route   PUT /api/posts
+// @access  Private
+const updatePost = (req, res) => {
+  res.status(200).json({ message: `Update Post ${req.params.id}` });
+};
+
+// @desc    Delete Post
+// @route   DELETE /api/posts
+// @access  Private
+const deletePost = (req, res) => {
+  res.status(200).json({ message: `Update Post ${req.params.id}` });
+};
+
+// exports
+module.exports = {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { getPosts, createPost, updatePost, deletePost } = require("../controllers/postController");
 
 // routes
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get Post" });
-});
+router.get("/", getPosts);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Create Post" });
-});
+router.post("/", createPost);
 
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update Post ${req.params.id}` });
-});
+router.put("/:id", updatePost);
 
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete Post ${req.params.id}` });
-});
-
+router.delete("/:id", deletePost);
 
 // Export the router
 module.exports = router;
