89ff80a419e0354264cacf88f28ca69a7b24895c
MNlegion <rkreuser30@gmail.com>
2024-01-31 17:02:54 -0500

    route initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // Initialize express
 const app = express();
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the server" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
