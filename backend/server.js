12bb86d5f6a9c4d15ef684e0b91fc25313b4320e
MNlegion <rkreuser30@gmail.com>
2024-01-30 23:00:14 -0500

    express setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello World!')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// Initialize express
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
