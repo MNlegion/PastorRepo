db89812958d25f9480f211f1ce90631f745612f0
MNlegion <rkreuser30@gmail.com>
2024-01-31 19:15:11 -0500

    config and mongodb setup

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get all posts
 // @route   GET /api/posts
 // @access  Private
-const getPosts = (req, res) => {
+const getPosts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Post" });
-};
+});
 
 // @desc    Create Post
 // @route   POST /api/posts
 // @access  Private
-const createPost = (req, res) => {
+const createPost = asyncHandler(async (req, res) => {
   if (!req.body.title || !req.body.text) {
-    res.status(400)
+    res.status(400);
     throw new Error("Please enter a title and text field");
   }
 
   res.status(200).json({ message: "Create Post" });
-};
+});
 
 // @desc    Update Post
 // @route   PUT /api/posts
 // @access  Private
-const updatePost = (req, res) => {
+const updatePost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Post ${req.params.id}` });
-};
+});
 
 // @desc    Delete Post
 // @route   DELETE /api/posts
 // @access  Private
-const deletePost = (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Post ${req.params.id}` });
-};
+});
 
 // exports
 module.exports = {
