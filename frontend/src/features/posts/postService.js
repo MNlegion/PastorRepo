bf96ebdff6ffa3890b745995a801399fe532395a
MNlegion <rkreuser30@gmail.com>
2024-02-05 20:46:57 -0500

    initial post slice and service setup

---
+++ frontend/src/features/posts/postService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_URL = "/api/posts/";
+
+// getPosts: async () => {
+//     const response = await axios.get(API_URL);
+//     return response.data;
+//   },
+//   createPost: async (postData) => {
+//     const response = await axios.post(API_URL, postData);
+//     return response.data;
+//   },
+//   updatePost: async (postId, postData) => {
+//     const response = await axios.put(API_URL + postId, postData);
+//     return response.data;
+//   },
+//   deletePost: async (postId) => {
+//     const response = await axios.delete(API_URL + postId);
+//     return response.data;
+//   },
+
+const postService = {
+//   getPosts,
+//   createPost,
+//   updatePost,
+//   deletePost,
+};
+
+export default postService;
