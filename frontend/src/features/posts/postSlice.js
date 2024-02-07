390ca59eda059de46433c48a0d64c999c47155cd
MNlegion <rkreuser30@gmail.com>
2024-02-06 21:29:03 -0500

    postSlice setup with auth token verification

--- frontend/src/features/posts/postSlice.js
+++ frontend/src/features/posts/postSlice.js
@@ -9,11 +9,30 @@ const initialState = {
   message: "",
 };
 
+// Create new post
+export const createPost = createAsyncThunk(
+  "posts/create",
+  async (postData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await postService.createPost(postData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    reset: (state) => initialState
+    reset: (state) => initialState,
   },
 });
 
